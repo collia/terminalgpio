d15ab47718ce05717e190451cd9a81408fe69885
Nikolay <collia@email.ua>
2019-07-21 20:50:21 +0300

    Added hal pwm support. Now pwm are generated from HW. With bugs

--- Src/gpio.c
+++ Src/gpio.c
@@ -5,6 +5,10 @@
 #include "main.h"
 
 
+#define TIM_COUNTER_CLOCK 10000 
+
+TIM_HandleTypeDef TIM_PWM_handler[4]; // TIM1 TIM2 ....
+
 static void GPIO_pwm_config()
 {
 #define  PERIOD_VALUE       (uint32_t)(700 - 1)  /* Period Value  */
@@ -58,8 +62,8 @@ static void GPIO_pwm_config()
 
 
 
- /* Compute the prescaler value to have TIM2 counter clock equal to 2000000 Hz */
-  uhPrescalerValue = (uint32_t)(SystemCoreClock / 2000000) - 1;
+ /* Compute the prescaler value to have TIM2 counter clock equal to 100000 Hz */
+  uhPrescalerValue = (uint32_t)(SystemCoreClock / 100000) - 1;
   
   /*##-1- Configure the TIM peripheral #######################################*/
   /* -----------------------------------------------------------------------
@@ -101,10 +105,19 @@ static void GPIO_pwm_config()
        + Counter direction = Up
   */
 
+  /*
+  *    counter_clock = 100 kHz
+  *   output_clock = 50 Hz
+  *   ARR = (counter_clock / output_clock) - 1 = 2000-1 = 1999
+  *  duty = 50%
+  *  CCR = ARR * % / 100 = 1999*0.5 = 999
+  *    
+  */
+  
   TimHandle.Instance = TIM2;
 
   TimHandle.Init.Prescaler         = uhPrescalerValue;
-  TimHandle.Init.Period            = PERIOD_VALUE;
+  TimHandle.Init.Period            = 1999;//PERIOD_VALUE;
   TimHandle.Init.ClockDivision     = 0;
   TimHandle.Init.CounterMode       = TIM_COUNTERMODE_UP;
   TimHandle.Init.RepetitionCounter = 0;
@@ -126,7 +139,7 @@ static void GPIO_pwm_config()
   sConfig.OCIdleState  = TIM_OCIDLESTATE_RESET;
   
   /* Set the pulse value for channel 1 */
-  sConfig.Pulse = PULSE1_VALUE;
+  sConfig.Pulse = 999;//PULSE1_VALUE;
   if (HAL_TIM_PWM_ConfigChannel(&TimHandle, &sConfig, TIM_CHANNEL_1) != HAL_OK)
   {
       /* Configuration Error */
@@ -134,7 +147,7 @@ static void GPIO_pwm_config()
   }
   
   /* Set the pulse value for channel 2 */
-  sConfig.Pulse = PULSE2_VALUE;
+  sConfig.Pulse = 333;//PULSE2_VALUE;
   if (HAL_TIM_PWM_ConfigChannel(&TimHandle, &sConfig, TIM_CHANNEL_2) != HAL_OK)
   {
       /* Configuration Error */
@@ -143,7 +156,7 @@ static void GPIO_pwm_config()
   
   
   /* Set the pulse value for channel 3 */
-  sConfig.Pulse = PULSE3_VALUE;
+  sConfig.Pulse = 1700;//PULSE3_VALUE;
   if (HAL_TIM_PWM_ConfigChannel(&TimHandle, &sConfig, TIM_CHANNEL_3) != HAL_OK)
   {
     /* Configuration Error */
@@ -151,7 +164,7 @@ static void GPIO_pwm_config()
   }
 
   /* Set the pulse value for channel 4 */
-  sConfig.Pulse = PULSE4_VALUE;
+  sConfig.Pulse = 1999;//PULSE4_VALUE;
   if (HAL_TIM_PWM_ConfigChannel(&TimHandle, &sConfig, TIM_CHANNEL_4) != HAL_OK)
   {
     /* Configuration Error */
@@ -210,8 +223,8 @@ static void GPIO_set_gpio_pwm_mode(GPIO_TypeDef *port, int gpio)
 {
     GPIO_InitTypeDef  GPIO_InitStruct;
 
-    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull  = GPIO_PULLDOWN;
+    GPIO_InitStruct.Mode  = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull  = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
     
     GPIO_InitStruct.Pin = gpio;
@@ -219,9 +232,37 @@ static void GPIO_set_gpio_pwm_mode(GPIO_TypeDef *port, int gpio)
 
 }
 
-static void GPIO_set_tim_pwm_mode(TIM_TypeDef *tim, int chan, int duty)
+static int GPIO_set_tim_pwm_mode(TIM_HandleTypeDef *handle, int chan, int duty)
 {
+    unsigned int period = handle->Init.Period;
+    TIM_OC_InitTypeDef sConfig;
+
+    if (HAL_TIM_PWM_Init(handle) != HAL_OK)
+    {
+        return -1;
+    }
     
+    sConfig.OCMode       = TIM_OCMODE_PWM1;
+    sConfig.OCPolarity   = TIM_OCPOLARITY_HIGH;
+    sConfig.OCFastMode   = TIM_OCFAST_DISABLE;
+    sConfig.OCNPolarity  = TIM_OCNPOLARITY_HIGH;
+    sConfig.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  
+    sConfig.OCIdleState  = TIM_OCIDLESTATE_RESET;
+  
+
+    sConfig.Pulse = period*duty/100;
+    if (HAL_TIM_PWM_ConfigChannel(handle, &sConfig, chan) != HAL_OK)
+    {
+        /* Configuration Error */
+        return -1;
+    }
+    if (HAL_TIM_PWM_Start(handle, chan) != HAL_OK)
+    {
+        /* PWM Generation Error */
+        return -1;
+    }
+    return 0;
 }
 
 static int GPIO_gpio_convert_to_hal(TERM_gpio_port_info_TYP *info,
@@ -363,6 +404,7 @@ int GPIO_init(TERM_gpio_port_info_TYP* gpio_table,
     __HAL_RCC_GPIOB_CLK_ENABLE();
     __HAL_RCC_GPIOC_CLK_ENABLE();
     __HAL_RCC_GPIOD_CLK_ENABLE();
+    __HAL_RCC_TIM2_CLK_ENABLE();
         
     while((gpio_line->idx.port != 0) &&
           (gpio_line->idx.line != 0))
@@ -382,7 +424,7 @@ int GPIO_init(TERM_gpio_port_info_TYP* gpio_table,
         pwm++;
     }
     
-    GPIO_pwm_config();
+    //GPIO_pwm_config();
     return 0;
 }
 
@@ -393,7 +435,9 @@ int GPIO_set_mode(TERM_gpio_port_info_TYP* gpio_line)
     GPIO_TypeDef *port;
     TIM_TypeDef *tim;
     int chan;
-
+    TIM_HandleTypeDef *timHandle;
+    
+    
     rc = GPIO_gpio_convert_to_hal(gpio_line, &port, &gpio);
     if(rc < 0)
         return rc;
@@ -410,8 +454,9 @@ int GPIO_set_mode(TERM_gpio_port_info_TYP* gpio_line)
         if(rc < 0)
             return rc;
 
+        timHandle = &TIM_PWM_handler[(int)gpio_line->pwm_info->tim-1];
         GPIO_set_gpio_pwm_mode(port, gpio);
-        GPIO_set_tim_pwm_mode(tim, chan, gpio_line->duty);
+        GPIO_set_tim_pwm_mode(timHandle, chan, gpio_line->duty);
         
     }
 
@@ -421,5 +466,34 @@ int GPIO_set_mode(TERM_gpio_port_info_TYP* gpio_line)
 
 extern int GPIO_pwm_cfg(TERM_gpio_tim_pwm_info_TYP* pwm_line)
 {
+
+    TIM_TypeDef *tim;
+    int chan;
+    TIM_HandleTypeDef *timHandle = &TIM_PWM_handler[(int)pwm_line->tim-1];
+    uint32_t uhPrescalerValue = 0;
+    unsigned int period;
+    int rc;
+
+    
+    rc = GPIO_tim_convert_to_hal(pwm_line,
+                                 &pwm_line->channels[0],
+                                 &tim,
+                                 &chan);
+    if(rc < 0)
+            return rc;
+
+    period = (TIM_COUNTER_CLOCK/pwm_line->freq)-1;
+    
+    timHandle->Instance = tim;
+    timHandle->Init.Prescaler         = uhPrescalerValue;
+    timHandle->Init.Period            = period;//1999;//PERIOD_VALUE;
+    timHandle->Init.ClockDivision     = 0;
+    timHandle->Init.CounterMode       = TIM_COUNTERMODE_UP;
+    timHandle->Init.RepetitionCounter = 0;
+    timHandle->Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+    if (HAL_TIM_PWM_Init(timHandle) != HAL_OK)
+    {
+        return -1;
+    }
     return 0;
 }
