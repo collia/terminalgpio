7db88bbcdce792e03670922750709a38bacb1c8e
Nikolay <collia@email.ua>
2019-08-05 22:29:24 +0300

    Iniital version for freq_calc algorithm

--- src/gpio_tim.c
+++ src/gpio_tim.c
@@ -9,11 +9,25 @@
  * testing
  */
 
-#include "stdint.h"
+#include <stdint.h>
+#include <stdbool.h>
 #include "gpio_tim.h"
-
+#include <stdio.h>
 #define TIM_COUNTER_CLOCK 10000
 
+static int calculate_freq(uint32_t system_clock, uint32_t prescaler, uint32_t period, uint32_t clock_division)
+{
+    uint32_t tim_tick_hz =  system_clock / (prescaler+1);
+    if(tim_tick_hz == 0)
+    {
+        return 0;
+    }
+    uint32_t pwm_hz = 10*tim_tick_hz / (period + 1);
+
+    return (pwm_hz);
+}
+
+//#define _OLD_
 /**
  * @brief         Funciton calculates paramters for tim 
  * @param[in]     system_clock
@@ -30,10 +44,56 @@ int GPIO_calc_tim_pwm_params(uint32_t system_clock,
                              uint32_t *period,
                              uint32_t *clock_division)
 {
+#ifdef _OLD_
     *prescaler = (uint32_t)(system_clock / TIM_COUNTER_CLOCK) - 1;
-
     *period = (((10*TIM_COUNTER_CLOCK)/freq))-1;
-
     *clock_division = 0;
     return 0;
+#else
+    int i;
+    uint32_t new_prescaler;
+    uint32_t new_period;
+    bool is_found = false;
+
+    for(i = 1; i < 0xffff; i+=1)
+    {
+        new_prescaler = (uint32_t)(system_clock / i) - 1;
+        if((new_prescaler == 0) || (new_prescaler >= 0xffff))
+        {
+            //i *= 100;
+            continue;
+        }
+
+        new_period = (((10*i)/freq));
+        if((new_period == 0) || (new_period >= 0xffff))
+        {
+            //i *= 100;
+            continue;
+        }
+
+        if(new_period < 100)
+        {
+            //i *= 10;
+            continue;
+        }
+
+        if(freq != calculate_freq(system_clock, new_prescaler, new_period, 0))
+        {
+            // printf("i=%d psc=%d, p=%d\n", i, new_prescaler, new_period);
+            //printf("%d!=%d\n",freq,calculate_freq(system_clock, new_prescaler, new_period, 0) );
+            continue;
+        }
+        //printf("i=%d sc=%d, p=%d\n", i, new_prescaler, new_period);
+        is_found = true;
+        *prescaler = new_prescaler;
+        *period = new_period;
+        *clock_division = 0;
+    }
+
+    if(is_found)
+        return 0;
+    else
+        return -1;
+#endif
+
 }
--- utest/timconfigutest.c
+++ utest/timconfigutest.c
@@ -18,8 +18,8 @@ static int calculate_freq(uint32_t prescaler, uint32_t period, uint32_t clock_di
 }
 
 
-static bool test_pwm_50_0_Hz(){
-    bool result = true;
+
+static bool test_pwm_positive(int waited_freq){
     int rc = 0;
     int back_rc = 0;
     uint32_t prescaler = 0;
@@ -27,22 +27,78 @@ static bool test_pwm_50_0_Hz(){
     uint32_t clock_division = 0;
     
     rc = GPIO_calc_tim_pwm_params(SystemCoreClock,
-                                  500, /* 50.0 */
+                                  waited_freq,
                                   &prescaler,
                                   &period,
                                   &clock_division);
     
-    PRINT_RESULT(rc == 0);
+    if(rc != 0)
+    {
+        printf("Freq=%d, rc = %d\n", waited_freq, rc);
+        return false;
+    }
     back_rc = calculate_freq(prescaler, period, clock_division);
-    PRINT_RESULT(back_rc == 500);
-    
+    if(prescaler == 0 || prescaler > 0xffff)
+    {
+        printf("incorrect prescaler(%d): 0x%x\n", waited_freq, prescaler);
+        return false;
+    }
+    if(period == 0 || period > 0xffff)
+    {
+        printf("incorrect period: 0x%x\n", period);
+        return false;
+    }
+    if(clock_division > 0xffff)
+    {
+        printf("incorrect clock_division: 0x%x\n", clock_division);
+        return false;
+    }
+    if(back_rc != waited_freq)
+    {
+        printf("waited_freq %d != %d\n", waited_freq, back_rc);
+        return false;
+    }
+
+    return true;
+}
+
+static bool test_pwm_50_0_Hz(){
+    bool result = true;
+    PRINT_RESULT(test_pwm_positive(500));
     return result;
 }
 
+static bool test_pwm_10_0_to_100_0Hz(){
+    bool result = true;
+    int i;
+    for(i = 100; i < 1000; i++)
+    {
+        result = test_pwm_positive(i);
+        if(!result)
+            break;
+    }
+    PRINT_RESULT(result);
+    return result;
+}
+
+static bool test_pwm_0_1_to_10_0Hz(){
+    bool result = true;
+    int i;
+    for(i = 1; i < 100; i++)
+    {
+        result = test_pwm_positive(i);
+        if(!result)
+            break;
+    }
+    PRINT_RESULT(result);
+    return result;
+}
 
 /* Tested API */
 bool pwm_params_buffer_utests() {
     bool rc = true;
     rc &= test_pwm_50_0_Hz();
+    rc &= test_pwm_10_0_to_100_0Hz();
+    rc &= test_pwm_0_1_to_10_0Hz();
     return rc;
 }
