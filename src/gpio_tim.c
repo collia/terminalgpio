212ded49f1bd914a0043378ef7faafaab7ad918a
Nikolay <collia@email.ua>
2019-08-09 21:46:10 +0300

    Improved freq algo. Now it works up tp 6kHz, and up to 1900 Hz with steps 0.1Hz

--- src/gpio_tim.c
+++ src/gpio_tim.c
@@ -12,18 +12,18 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "gpio_tim.h"
-#include <stdio.h>
+//#include <stdio.h>
 #define TIM_COUNTER_CLOCK 10000
 
 static unsigned int rounded_div(unsigned int a, unsigned int b)
 {
-	unsigned int rem = a%b;
-	unsigned int quot = a/b;
-	if(rem > (b >> 1))
-	{
-		quot += 1;
-	}
-	return quot;
+    unsigned int rem = a%b;
+    unsigned int quot = a/b;
+    if(rem > (b >> 1))
+    {
+        quot += 1;
+    }
+    return quot;
 }
 
 static int calculate_freq(uint32_t system_clock, uint32_t prescaler, uint32_t period, uint32_t clock_division)
@@ -40,7 +40,7 @@ static int calculate_freq(uint32_t system_clock, uint32_t prescaler, uint32_t pe
 
 //#define _OLD_
 /**
- * @brief         Funciton calculates paramters for tim 
+ * @brief         Funciton calculates paramters for tim
  * @param[in]     system_clock
  * @param[in]     freq - needed frequency for pwm
  * @param[out]    prescaler
@@ -65,44 +65,38 @@ int GPIO_calc_tim_pwm_params(uint32_t system_clock,
     uint32_t new_prescaler;
     uint32_t new_period;
     bool is_found = false;
+    int increment = freq;
+    if(freq > 5000)
+        increment /= 10;
 
-    for(i = 1; i < 0xffff; i+=1)
+    for(i = 1; i < system_clock; i+=increment)
     {
         new_prescaler = (uint32_t)(rounded_div(system_clock , i)) - 1;
         if((new_prescaler == 0) || (new_prescaler >= 0xffff))
         {
-            //i *= 100;
-            //printf("i=%d 1 psc=%d\n", i, new_prescaler);
             continue;
         }
 
         new_period = (rounded_div((10*i), freq)) -1;
         if((new_period == 0) || (new_period >= 0xffff))
         {
-            //i *= 100;
-	    //printf("i=%d 1 p=%d\n", i, new_period);
             continue;
         }
 
-        if(new_period < 249)
+        if(new_period < 100)
         {
-            //i *= 10;
-	    //printf("i=%d 2 p=%d\n", i, new_period);
             continue;
         }
 
         if(freq != calculate_freq(system_clock, new_prescaler, new_period, 0))
         {
-            //printf("i=%d psc=%d, p=%d\n", i, new_prescaler, new_period);
-            //printf("%d!=%d\n",freq,calculate_freq(system_clock, new_prescaler, new_period, 0) );
             continue;
         }
-        //printf("i=%d sc=%d, p=%d\n", i, new_prescaler, new_period);
         is_found = true;
         *prescaler = new_prescaler;
         *period = new_period;
         *clock_division = 0;
-	break;
+        break;
     }
 
     if(is_found)
--- utest/timconfigutest.c
+++ utest/timconfigutest.c
@@ -10,29 +10,26 @@
 
 #define SystemCoreClock  72000000U
 
-static int calculate_freq(uint32_t prescaler, uint32_t period, uint32_t clock_division)
+static double calculate_freq(uint32_t prescaler, uint32_t period, uint32_t clock_division)
 {
     double tim_tick_hz = (double)SystemCoreClock / (double)(prescaler+1);
     double freq = (10* tim_tick_hz) / (period + 1);
-    //printf("tick: %f freq=%f\n", tim_tick_hz, freq);
-    return (int)round(freq);
+    return (freq);
 }
 
 
 
-static bool test_pwm_positive(int waited_freq, int error){
+static bool test_pwm_positive(int waited_freq, double error){
     int rc = 0;
-    int back_rc = 0;
+    double back_rc = 0;
     uint32_t prescaler = 0;
     uint32_t period = 0;
     uint32_t clock_division = 0;
-    
     rc = GPIO_calc_tim_pwm_params(SystemCoreClock,
                                   waited_freq,
                                   &prescaler,
                                   &period,
                                   &clock_division);
-    
     if(rc != 0)
     {
         printf("Freq=%d, rc = %d\n", waited_freq, rc);
@@ -57,16 +54,19 @@ static bool test_pwm_positive(int waited_freq, int error){
     if((back_rc < waited_freq-error) ||
        (back_rc > waited_freq+error))
     {
-        printf("waited_freq %d != %d\n", waited_freq, back_rc);
+        printf("waited_freq %d != %f +- %f\n", waited_freq, back_rc, error);
         return false;
     }
 
     return true;
 }
 
-static bool test_pwm_50_0_Hz(){
+static bool test_pwm_important(){
     bool result = true;
-    PRINT_RESULT(test_pwm_positive(500, 0));
+    PRINT_RESULT(test_pwm_positive(500, 0.15));
+    PRINT_RESULT(test_pwm_positive(600, 0.15));
+    PRINT_RESULT(test_pwm_positive(1000, 0.15));
+    PRINT_RESULT(test_pwm_positive(1200, 0.15));
     return result;
 }
 
@@ -75,7 +75,7 @@ static bool test_pwm_10_0_to_100_0Hz(){
     int i;
     for(i = 100; i < 1000; i++)
     {
-        result = test_pwm_positive(i, 0);
+        result = test_pwm_positive(i, 0.3);
         if(!result)
             break;
     }
@@ -88,7 +88,7 @@ static bool test_pwm_0_1_to_10_0Hz(){
     int i;
     for(i = 1; i < 100; i++)
     {
-        result = test_pwm_positive(i, 0);
+        result = test_pwm_positive(i, 0.3);
         if(!result)
             break;
     }
@@ -109,12 +109,43 @@ static bool test_pwm_100_0_to_1000_0Hz(){
     return result;
 }
 
+static bool test_pwm_1000_0_to_19345_0Hz(){
+    bool result = true;
+    int i;
+    for(i = 10000; i < 19346; i++)
+    {
+        result = test_pwm_positive(i, 1);
+        if(!result)
+            break;
+    }
+    PRINT_RESULT(result);
+    return result;
+}
+
+static bool test_pwm_hi_freq(){
+    bool result = true;
+    PRINT_RESULT(test_pwm_positive(2000, 1));
+
+    PRINT_RESULT(test_pwm_positive(2500, 1));
+    PRINT_RESULT(test_pwm_positive(3000, 1));
+    PRINT_RESULT(test_pwm_positive(3500, 1));
+    PRINT_RESULT(test_pwm_positive(4000, 1));
+    PRINT_RESULT(test_pwm_positive(4500, 1));
+    PRINT_RESULT(test_pwm_positive(5000, 1));
+    PRINT_RESULT(test_pwm_positive(6000, 1));
+
+    return result;
+}
+
 /* Tested API */
 bool pwm_params_buffer_utests() {
     bool rc = true;
-    rc &= test_pwm_50_0_Hz();
-    //rc &= test_pwm_10_0_to_100_0Hz();
-    //rc &= test_pwm_0_1_to_10_0Hz();
+    rc &= test_pwm_important();
+    rc &= test_pwm_10_0_to_100_0Hz();
+    rc &= test_pwm_0_1_to_10_0Hz();
     rc &= test_pwm_100_0_to_1000_0Hz();
+    rc &= test_pwm_1000_0_to_19345_0Hz();
+    rc &= test_pwm_hi_freq();
+
     return rc;
 }
